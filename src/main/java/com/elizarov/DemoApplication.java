31f042d3de8b69185e0198323c37d5dba83f6ccf
Aleksandr Elizarov <elezar_87@mail.ru>
2023-02-03 14:02:51 +0300

    First drop

---
+++ src/main/java/com/elizarov/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.elizarov;
+
+import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+//		SpringApplication.run(DemoApplication.class, args);
+        Application.launch(JavaFxApplication.class, args);
+    }
+
+}
